050e229376e5221920cdafd4795d24c73285675d
hamalbigbluedoor <hamal@bigbluedoor.net>
2021-01-08 15:50:57 +0000

    Upload image form

--- gallery.php
+++ gallery.php
@@ -6,9 +6,35 @@
 <section class="gallery">	
   <div class="product">
 
-<?php allImages(); ?>
+  <?php 
+  allImages();
+  ?>
 
   </div>
 </section>
+
+<?php add_image(); ?>
+
+<form action="" method="post" enctype="multipart/form-data" class="image-form">
+  <h2>Upload your Image Now!</h2>
+  <div class="form-group">
+    <label for="image">Choose your local Image:</label>
+    <br>
+    <input type="file" class="form-control" name="image">
+  </div>
+  <br>
+  <div class="form-group">
+    <!-- A select list to dynamically display all categories in database -->
+    <label for="image_subject">Please select a subject for your image:</label>
+    <br>
+    <select name="image_subject" id="image_subject">
+      <?php subjectSelectList(); ?>
+    </select>
+  </div>
+  <br>
+  <div class="form-group">
+    <input type="submit" class="btn btn-primary" name="upload_image" value="Upload Image">
+  </div>
+</form>
 
 <?php include "./includes/footer.php" ?>
